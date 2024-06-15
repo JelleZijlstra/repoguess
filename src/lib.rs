8d6ae5478cb7e355bb8f9867cc39f85f92519e62
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-06-15 13:40:10 -0700

    Change approach a bit

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ struct NameData {
     collection: i32,
     tl_country: i32,
     year: i32,
-    single_author: i32,
-    authors: std::collections::HashSet<i32>,
+    authors: Vec<i32>,
     citation_group: i32,
     name_id: i32,
 }
@@ -18,8 +17,7 @@ impl NameData {
         collection: i32,
         tl_country: i32,
         year: i32,
-        single_author: i32,
-        authors: std::collections::HashSet<i32>,
+        authors: Vec<i32>,
         citation_group: i32,
         name_id: i32,
     ) -> Self {
@@ -27,7 +25,6 @@ impl NameData {
             collection,
             tl_country,
             year,
-            single_author,
             authors,
             citation_group,
             name_id,
@@ -49,6 +46,7 @@ struct Params {
     author_boost: f64,
     year_factor: f64,
     score_cutoff: f64,
+    probability_cutoff: f64,
 }
 
 #[pymethods]
@@ -60,6 +58,7 @@ impl Params {
         author_boost: f64,
         year_factor: f64,
         score_cutoff: f64,
+        probability_cutoff: f64,
     ) -> Self {
         Params {
             country_boost,
@@ -67,13 +66,14 @@ impl Params {
             author_boost,
             year_factor,
             score_cutoff,
+            probability_cutoff,
         }
     }
 
     fn __repr__(&self) -> PyResult<String> {
         Ok(format!(
-            "Params(country_boost={}, cg_boost={}, author_boost={}, year_factor={}, score_cutoff={})",
-            self.country_boost, self.cg_boost, self.author_boost, self.year_factor, self.score_cutoff
+            "Params(country_boost={:.3}, cg_boost={:.3}, author_boost={:.3}, year_factor={:.3}, score_cutoff={:.3}, probability_cutoff={:.3})",
+            self.country_boost, self.cg_boost, self.author_boost, self.year_factor, self.score_cutoff, self.probability_cutoff
         ))
     }
 }
@@ -90,24 +90,18 @@ fn get_score(nam1: &NameData, nam2: &NameData, params: &Params) -> PyResult<f64>
     if nam1.citation_group == nam2.citation_group {
         score *= params.cg_boost;
     }
-    if (nam1.single_author != 0) && (nam2.single_author != 0) {
-        if nam1.single_author == nam2.single_author {
-            score *= params.author_boost;
-        }
-    }
-    else if nam1.single_author != 0 {
-        if nam2.authors.contains(&nam1.single_author) {
-            score *= params.author_boost / (nam2.authors.len() as f64);
-        }
-    }
-    else if nam2.single_author != 0 {
-        if nam1.authors.contains(&nam2.single_author) {
-            score *= params.author_boost / (nam1.authors.len() as f64);
-        }
+    if nam1.authors == nam2.authors {
+        score *= params.author_boost;
     }
     else {
-        let shared_authors = nam1.authors.intersection(&nam2.authors).count();
-        let total_authors = nam1.authors.union(&nam2.authors).count();
+        // Faster than using sets since usually it's a small number of authors
+        let mut shared_authors = 0;
+        for author in &nam1.authors {
+            if nam2.authors.contains(&author) {
+                shared_authors += 1;
+            }
+        }
+        let total_authors = nam1.authors.len() + nam2.authors.len() - shared_authors;
         let overlap_prop = (shared_authors as f64) / (total_authors as f64);
         if overlap_prop > 0.0 {
             score *= overlap_prop * params.author_boost;
@@ -147,14 +141,14 @@ fn get_probs_impl(data: &NameData, train_data: &Vec<Bound<'_, NameData>>, params
 }
 
 #[pyfunction]
-fn get_top_choice(data: &NameData, train_data: Vec<Bound<'_, NameData>>, params: &Params, cutoff: f64) -> PyResult<i32> {
-    return get_top_choice_impl(data, &train_data, params, cutoff);
+fn get_top_choice(data: &NameData, train_data: Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<i32> {
+    return get_top_choice_impl(data, &train_data, params);
 }
 
-fn get_top_choice_impl(data: &NameData, train_data: &Vec<Bound<'_, NameData>>, params: &Params, cutoff: f64) -> PyResult<i32> {
+fn get_top_choice_impl(data: &NameData, train_data: &Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<i32> {
     let probs = get_probs_impl(data, train_data, params)?;
     let mut top_choice: i32 = -1;
-    let mut top_score: f64 = cutoff;
+    let mut top_score: f64 = params.probability_cutoff;
     for (key, value) in probs.iter() {
         if *value > top_score {
             top_choice = *key;
@@ -166,17 +160,37 @@ fn get_top_choice_impl(data: &NameData, train_data: &Vec<Bound<'_, NameData>>, p
 
 const FALSE_POSITIVE_COST: i32 = 10;
 
+#[pyclass(get_all, frozen)]
+struct ScoreInfo {
+    score: i32,
+    correct: i32,
+    incorrect: i32,
+    no_value: i32,
+}
+
+#[pymethods]
+impl ScoreInfo {
+    fn __repr__(&self) -> PyResult<String> {
+        Ok(format!(
+            "ScoreInfo(score={}, correct={}, incorrect={}, no_value={})",
+            self.score, self.correct, self.incorrect, self.no_value
+        ))
+    }
+}
+
 #[pyfunction]
-fn evaluate_model(train_data: Vec<Bound<'_, NameData>>, test_data: Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<i32> {
+fn evaluate_model(train_data: Vec<Bound<'_, NameData>>, test_data: Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<ScoreInfo> {
     return evaluate_model_impl(&train_data, &test_data, params);
 }
 
-fn evaluate_model_impl(train_data: &Vec<Bound<'_, NameData>>, test_data: &Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<i32> {
+fn evaluate_model_impl(train_data: &Vec<Bound<'_, NameData>>, test_data: &Vec<Bound<'_, NameData>>, params: &Params) -> PyResult<ScoreInfo> {
     let mut correct: i32 = 0;
     let mut incorrect: i32 = 0;
+    let mut no_value: i32 = 0;
     for nam in test_data {
-        let top_choice = get_top_choice_impl(nam.get(), train_data, params, 0.9)?;
+        let top_choice = get_top_choice_impl(nam.get(), train_data, params)?;
         if top_choice == -1 {
+            no_value += 1;
             continue;
         }
         if top_choice == nam.get().collection {
@@ -185,7 +199,15 @@ fn evaluate_model_impl(train_data: &Vec<Bound<'_, NameData>>, test_data: &Vec<Bo
             incorrect += 1;
         }
     }
-    return Ok(correct - (incorrect * FALSE_POSITIVE_COST));
+    let score = correct - (incorrect * FALSE_POSITIVE_COST);
+    return Ok(
+        ScoreInfo {
+            score,
+            correct,
+            incorrect,
+            no_value,
+        }
+    );
 }
 
 #[pymodule]
@@ -196,5 +218,6 @@ fn typeinfer(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(evaluate_model, m)?)?;
     m.add_class::<NameData>()?;
     m.add_class::<Params>()?;
+    m.add_class::<ScoreInfo>()?;
     Ok(())
 }
